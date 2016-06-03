9b976457d2ea06625a08c1fafde00aa78479f09a
Cristian Tabacitu <cristitabacitu@gmail.com>
2016-06-03 13:48:15 -0400

    Applied fixes from StyleCI
    [ci skip] [skip ci]

--- src/app/Http/routes.php
+++ src/app/Http/routes.php
@@ -6,5 +6,5 @@ Route::group(['prefix' => 'admin', 'middleware' => ['web', 'auth']], function ()
     Route::get('backup', 'BackupController@index');
     Route::put('backup/create', 'BackupController@create');
     Route::get('backup/download/{folder_name}/{file_name?}', 'BackupController@download');
-	Route::delete('backup/delete/{folder_name}/{file_name?}', 'BackupController@delete');
+    Route::delete('backup/delete/{folder_name}/{file_name?}', 'BackupController@delete');
 });
