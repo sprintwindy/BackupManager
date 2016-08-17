081ee0c23ad8d26759fe14ffce56ae195b8bc776
Cristian Tabacitu <cristitabacitu@gmail.com>
2016-08-17 01:19:20 -0400

    Applied fixes from StyleCI
    [ci skip] [skip ci]

--- src/resources/lang/es/backup.php
+++ src/resources/lang/es/backup.php
@@ -25,7 +25,7 @@ return [
     'delete_confirmation_message'        => 'La copia de seguridad fue eliminada.',
     'delete_error_title'                 => 'Ups, ha ocurrido un error',
     'delete_error_message'               => 'La copia de seguridad NO se pudo eliminar.',
-    'delete_cancel_title'                => "La operación ha sido cancelada",
+    'delete_cancel_title'                => 'La operación ha sido cancelada',
     'delete_cancel_message'              => 'La copia de seguridad NO se pudo eliminar.',
     'create_confirmation_title'          => 'Se ha completado la copia de seguridad',
     'create_confirmation_message'        => 'Recargando la página en 3 segundos.',
@@ -35,6 +35,6 @@ return [
     'create_warning_message'             => 'La copia de seguridad puede que no se haya realizado. Por favor verifica los logs para más detalles.',
     'location'                           => 'Ubicación',
     'no_disks_configured'                => 'No existe ningún disco configurado config/laravel-backup.php',
-    'backup_doesnt_exist'                => "La copia de seguridad no existe.",
+    'backup_doesnt_exist'                => 'La copia de seguridad no existe.',
     'only_local_downloads_supported'     => 'Solo se permiten descargas del sistema de archivos local.',
 ];
