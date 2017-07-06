798e5e5ed4a7a8be116aff4d1cb26dc0f0a59b9a
Cristian Tabacitu <cristitabacitu@gmail.com>
2017-07-06 05:34:38 +0000

    Apply fixes from StyleCI

--- src/routes/backpack/backupmanager.php
+++ src/routes/backpack/backupmanager.php
@@ -15,7 +15,6 @@ Route::group([
     'prefix'     => config('backpack.base.route_prefix', 'admin'),
     'middleware' => ['web', 'admin'],
 ], function () {
-
     Route::get('backup', 'BackupController@index');
     Route::put('backup/create', 'BackupController@create');
     Route::get('backup/download/{file_name?}', 'BackupController@download');
